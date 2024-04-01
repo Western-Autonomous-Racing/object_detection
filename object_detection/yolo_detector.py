29ab8355e3ac08b59e01c619834a1665ef5a7228
d4n13lx13 <danxie2001@gmail.com>
2024-04-01 00:19:42 -0400

    init yolo detector

--- object_detection/yolo_detector.py
+++ object_detection/yolo_detector.py
@@ -1,23 +1,48 @@
+from ultralytics import YOLO
+import cv2
 import numpy as np
 import torch
 
 '''
-    Use YOLOv8 with int8 quantization
+    Use YOLOv8
     min-z = 175    
 
 '''
 
 class YOLO_Detector:
 
-    def __init__(self, model_path, device):
+    def __init__(self, model_path):
         '''
         params:
         model_path: path to the model
         device: device to run the model
         '''
-        self.model = torch.load(model_path)
-        self.device = device
-        self.model.to(self.device)
-        self.model.eval()
+        self.model = YOLO(model_path)
 
-    
+    def detect(self, frame):
+        '''
+        params:
+        frame: input frame
+        return:
+        boxes: detected boxes
+        prob: detected probabilities
+        '''
+        results = self.model(frame)
+        boxes = []
+        conf = []
+        for r in results:
+            boxes.append(r.boxes.xywh)
+            conf.append(r.boxes.conf)
+        return boxes, conf
+        
+
+if __name__ == '__main__':
+    yolo = YOLO_Detector('yolov8s.pt')
+    frame = cv2.imread('test.jpg')
+    boxes, conf = yolo.detect(frame)
+    print(boxes)
+    print(conf)
+
+    for i in range(3) :
+        print(boxes[0][i])
+        print(conf[0][i])
